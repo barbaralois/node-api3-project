e27ef578e8182f174f0cdb8f02076d3f32ec5f86
Barbara Moore <barbaraloismoore@gmail.com>
2020-07-09 11:58:12 -0400

    completed stretch - added postRouter functionality

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,27 +1,61 @@
 const express = require('express');
 
+const Posts = require('./postDb.js');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  // do your magic!
+router.get('/', async (req, res) => {
+  try {
+    let allPosts = await Posts.get();
+    res.status(200).json(allPosts);
+  } catch {
+    res.status(500).json({ message: 'There was an error retrieving posts.' });
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // do your magic!
+router.get('/:id', validatePostId, async (req, res) => {
+  try {
+    let selected = await Posts.getById(req.post.id);
+    res.status(200).json(selected);
+  } catch {
+    res
+      .status(500)
+      .json({ message: 'The post information could not be retrieved' });
+  }
 });
 
-router.delete('/:id', (req, res) => {
-  // do your magic!
+router.delete('/:id', validatePostId, async (req, res) => {
+  try {
+    await Posts.remove(req.post.id);
+    res.status(200).json({ message: 'The post has been deleted.' });
+  } catch {
+    res.status(500).json({ message: 'There was an error deleting the post.' });
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // do your magic!
+router.put('/:id', validatePostId, async (req, res) => {
+  try {
+    await Posts.update(req.post.id, req.body);
+    res.status(200).json({ message: 'The post has been updated.' });
+  } catch {
+    res.status(500).json({ message: 'There was an error modifying the post.' });
+  }
 });
 
 // custom middleware
 
 function validatePostId(req, res, next) {
-  // do your magic!
+  const id = req.params.id;
+  Posts.getById(id)
+    .then((postId) => {
+      if (postId) {
+        req.post = postId;
+        next();
+      } else {
+        res.status(400).json({ message: 'invalid post id' });
+      }
+    })
+    .catch((err) => res.status(500).json({ message: 'error validating id' }));
 }
 
 module.exports = router;
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const userRouter = require('./users/userRouter.js');
 const postRouter = require('./posts/postRouter.js');
 
 server.use('/api/users', logger, userRouter);
-server.use('/api/posts', postRouter);
+server.use('/api/posts', logger, postRouter);
 
 server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
